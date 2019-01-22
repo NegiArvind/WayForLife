5a64a93ec2b8932ee2fbde41f3f1d1dd7cee17bc
arvind <negiarvind229@gmail.com>
2019-01-22 23:07:15 +0530

    Authentication completed

--- app/src/main/java/com/wayforlife/Fragments/LoginFragment.java
+++ app/src/main/java/com/wayforlife/Fragments/LoginFragment.java
@@ -18,12 +18,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.kaopiz.kprogresshud.KProgressHUD;
 import com.wayforlife.Activities.LoginActivity;
 import com.wayforlife.GlobalStateApplication;
 import com.wayforlife.Models.User;
@@ -31,7 +25,6 @@ import com.wayforlife.R;
 import com.wayforlife.Utils.AuthUtil;
 import com.wayforlife.Utils.ProgressUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
